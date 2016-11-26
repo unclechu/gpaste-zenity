d0f5f4cd25390c48caf68f8d62d4c2a881203dbd
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2016-11-26 23:30:44 +0500

    support 'choose' mode to choose action by gui dialog

--- gpaste-zenity.sh
+++ gpaste-zenity.sh
@@ -8,12 +8,25 @@ exec 0</dev/null
 CONTENTS_LIMIT=80
 LINE_MORE='...'
 REAL_CONTENTS_LIMIT=$[$CONTENTS_LIMIT-${#LINE_MORE}]
-MODE=select
+WND_TITLE=gpaste-zenity
+
+# could be changed by 'choose' mode
+mode=select
+
+GPASTE_BIN=$(\
+	(which gpaste-client 1>&- 2>&- && echo 'gpaste-client') || \
+	(which gpaste        1>&- 2>&- && echo 'gpaste'       ) || \
+	exit 1
+	)
+if [ $? -ne 0 ]; then
+	echo 'GPaste client tool not found' 1>&2
+	exit 1
+fi
 
 for opt in "$@"; do
 	case $opt in
 		-m=*|--mode=*)
-			MODE="${opt#*=}"
+			mode="${opt#*=}"
 			shift
 			;;
 		*)
@@ -23,31 +36,18 @@ for opt in "$@"; do
 	esac
 done
 
-case $MODE in
-	select|delete)
+case "$mode" in
+	select|delete|choose)
 		;;
 	*)
-		echo "Unknown mode: '$MODE'" 1>&2
+		echo "Unknown mode: '$mode'" 1>&2
 		exit 1
 		;;
 esac
 
-gpaste_bin=
-if which gpaste-client 1>&- 2>&-; then
-	gpaste_bin=gpaste-client
-elif which gpaste 1>&- 2>&-; then
-	gpaste_bin=gpaste
-else
-	echo 'GPaste not found' 1>&2
-	exit 1
-fi
-
 catch_fak() {
 	[ $1 -ne 0 ] && { echo fak 1>&2; exit 1; }
 }
-
-gpaste_list=$("$gpaste_bin" history --oneline 2>&-)
-catch_fak $?
 
 clear_line() {
 	echo "$1" \
@@ -79,46 +79,88 @@ delete_items() {
 	while read id; do
 		[ "$id" == "" ] && return 1
 		local name="__marked_to_delete_$id"
-		"$gpaste_bin" set-password "$id" "$name" 0<&- 1>&- 2>&-
+		"$GPASTE_BIN" set-password "$id" "$name" 0<&- 1>&- 2>&-
 		if [ $? -ne 0 ]; then
-			local given_name=$("$gpaste_bin" get "$id" 0<&- 2>&-)
+			local given_name=$("$GPASTE_BIN" get "$id" 0<&- 2>&-)
 			[ "${given_name:0:11}" != '[Password] ' ] && return 1
 			local old_name=${given_name:11}
-			"$gpaste_bin" rename-password "$old_name" "$name" 0<&- 1>&- 2>&-
+			"$GPASTE_BIN" rename-password "$old_name" "$name" 0<&- 1>&- 2>&-
 			[ $? -ne 0 ] && return 1
 		fi
 		names+=("$name")
 	done
 	for name in "${names[@]}"; do
-		"$gpaste_bin" delete-password "$name" 0<&- 1>&- 2>&-
+		"$GPASTE_BIN" delete-password "$name" 0<&- 1>&- 2>&-
 		[ $? -ne 0 ] && return 1
 	done
 	return 0
 }
 
-title="GPaste ($MODE)"
+choose_mode() {
+	local modes=( \
+		"Select" "select" \
+		"Delete" "delete"
+		)
+	local chosen_mode=$(printf "|%s" "${modes[@]}" \
+		| cut -b '2-' \
+		| tr '|' '\n' \
+		| zenity \
+			--width 320 --height 240 \
+			--title "$WND_TITLE" \
+			--text 'GPaste (choose action)' \
+			--list \
+			--print-column=2 \
+			--hide-column=2 \
+			--hide-header \
+			--mid-search \
+			--column 'Action' --column='System action' \
+			2>/dev/null
+		)
+	([ $? -ne 0 ] || [ "$chosen_mode" == "" ]) && return 1
+	echo "$chosen_mode"
+}
+
+
+gpaste_list=$("$GPASTE_BIN" history --oneline 2>&-)
+catch_fak $?
+
+if [ "$gpaste_list" == "" ]; then
+	zenity \
+		--width 200 \
+		--title "$WND_TITLE" \
+		--warning --text="Clipboard history is empty"
+	exit 1
+fi
+
+
+if [ "$mode" == 'choose' ]; then
+	mode=$(choose_mode)
+	[ $? -ne 0 ] && exit 1
+fi
+
+
+title="GPaste ($mode)"
 
 choose=$(echo "$gpaste_list" \
 	| gen_pipe \
 	| zenity \
-		--title 'gpaste-zenity' \
+		--width 800 --height 600 \
+		--title "$WND_TITLE" \
 		--text "$title" \
 		--list \
-		--width 800 \
-		--height 600 \
 		--print-column=2 \
 		--hide-column=2 \
 		--hide-header \
 		--mid-search \
-		$([ "$MODE" == 'delete' ] && echo --multiple) \
+		$([ "$mode" == 'delete' ] && echo --multiple) \
 		--column 'Contents' --column '#' \
 		2>/dev/null
 	)
 ([ $? -ne 0 ] || [ "$choose" == "" ]) && exit 1
 
-if [ "$MODE" == 'delete' ]; then
+if [ "$mode" == 'delete' ]; then
 	echo "$choose" | tr '|' '\n' | delete_items
 else
-	"$gpaste_bin" "$MODE" "$choose"
+	"$GPASTE_BIN" "$mode" "$choose"
 fi
 catch_fak $?
